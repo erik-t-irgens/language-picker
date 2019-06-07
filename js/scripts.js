0c011362b8122a3de91a372ba436398134b41885
Student <example@example.com>
2019-06-07 11:05:06 -0700

    Add bootstrap.js for extra functionality, add more formatting, more questions, and give them values for later functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -12,14 +12,34 @@ $(document).ready(function() {
 
 // Each var below here has an if statement that determines the answer of the question by pulling the value (1, 2, 3 etc depending on amount of answers), then defines what happens when that number is met .In this case, a 1 (for beginner in the question) adds +1 to the above variables for the final verdict.)
     var question1 = $("input:radio[name=difficulty]:checked").val();
-    if (question1 === 1){
-       python = python + 1;
-      javaScript = javaScript + 0;
-      cSharp = cSharp + 0;
-      rust = rust + 0;
-      ruby = ruby + 0;
-      html = html + 0;
-    }
+      if (question1 === 1){
+
+        python = python + 1;
+        javaScript = javaScript + 0;
+        cSharp = cSharp + 0;
+        rust = rust + 0;
+        ruby = ruby + 0;
+        html = html + 0;
+
+      } else if (question1 === 2) {
+
+        python = python + 0;
+        javaScript = javaScript + 0;
+        cSharp = cSharp + 0;
+        rust = rust + 0;
+        ruby = ruby + 0;
+        html = html + 0;
+
+      } else if (question1 === 3) {
+
+        python = python + 0;
+        javaScript = javaScript + 0;
+        cSharp = cSharp + 0;
+        rust = rust + 0;
+        ruby = ruby + 0;
+        html = html + 0;
+
+      }
 
 
     var question2 = $("input:radio[name=difficulty]:checked").val();
@@ -27,5 +47,28 @@ $(document).ready(function() {
       total
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    event.preventDefault();
   });
 });
