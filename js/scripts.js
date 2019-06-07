72c7a0064d398ad036d53b6d533cc657a118767a
Student <example@example.com>
2019-06-07 14:05:47 -0700

    Fix functionality in buttons next/previous. Add way to check highest scoring item and display it.

--- js/scripts.js
+++ js/scripts.js
@@ -7,75 +7,73 @@ $(document).ready(function() {
     // Main page start button
         $("#startButton").click(function(){
           $("#languageForm").toggle();
+          $("#question1").slideDown();
         });
     // question 1 buttons
         $("#question1next").click(function(){
           $("#question1").slideUp();
-          $("#question2").show();
+          $("#question2").slideDown();
         });
-        $("#question1back").click(function(){
-          $("#question1").hide();
-        });
+
 
       // question 2 buttons
-
         $("#question2next").click(function(){
-          $("#question2").slideup();
-          $("#question3").show();
+          $("#question2").slideUp();
+          $("#question3").slideDown();
         });
         $("#question2back").click(function(){
           $("#question1").slideDown();
-          $("#question2").hide();
+          $("#question2").slideUp();
         });
 
       // question 3 buttons
 
         $("#question3next").click(function(){
           $("#question3").slideUp();
-          $("#question4").show();
+          $("#question4").slideDown();
         });
-        $("#question2back").click(function(){
+        $("#question3back").click(function(){
           $("#question2").slideDown();
-          $("#question3").hide();
+          $("#question3").slideUp();
         });
 
     // question 4 buttons
         $("#question4next").click(function(){
           $("#question4").slideUp();
-          $("#question5").show();
+          $("#question5").slideDown();
         });
         $("#question4back").click(function(){
           $("#question3").slideDown();
-          $("#question4").hide();
+          $("#question4").slideUp();
         });
 
       // question 5 buttons
 
         $("#question5next").click(function(){
           $("#question5").slideUp();
-          $("#question6").show();
+          $("#question6").slideDown();
         });
         $("#question5back").click(function(){
           $("#question4").slideDown();
-          $("#question5").hide();
+          $("#question5").slideUp();
         });
 
       // question 6 buttons
 
         $("#question6next").click(function(){
           $("#question6").slideUp();
-          $("#submitStyle").show();
+          $("#submitStyle").slideDown();
         });
         $("#question6back").click(function(){
           $("#question5").slideDown();
-          $("#question6").hide();
+          $("#question6").slideUp();
         });
 
       // Submit buttons
 
         $("#submitBack").click(function(){
-          $("#question6").show();
-          $("#submitStyle").hide();
+          $("#question6").slideUp();
+          $("#submitStyle").slideDown();
         });
 
   $("#languageForm").submit(function(event) {
@@ -92,7 +90,7 @@ $(document).ready(function() {
   var ruby = 0;
   var html = 0;
 
-  debugger;
+  
   var question1 = parseInt($("input:radio[name=question1]:checked").val());
   var question2 = parseInt($("input:radio[name=question2]:checked").val());
   var question3 = parseInt($("input:radio[name=question3]:checked").val());
@@ -177,6 +175,30 @@ $(document).ready(function() {
   } else {
   }
 
+// Here, I have the value of the original variable looked at. I've decided that, if you rank in any of these languages with 3 or more, it will display all ranked that high you! If not, it won't display it.
+  if (python >= 3){
+    $("#suggestionPython").slideDown();
+  }
+
+  if (javaScript >=3) {
+    $("#suggestionJavaScript").slideDown();
+  }
+
+  if (cSharp >=3) {
+     $("#suggestionCSharp").slideDown();
+  }
+
+  if (rust >=3) {
+    $("#suggestionRust").slideDown();
+  }
+
+  if (ruby >=3) {
+    $("#suggestionRuby").slideDown();
+  }
+
+  if (html >= 3) {
+    $("#suggestionHtml").slideDown();
+  }
     event.preventDefault();
   });
 });
